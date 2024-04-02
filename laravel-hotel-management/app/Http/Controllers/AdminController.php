cbbcdbcdc30fc4b6ad3e3ccf18e04b55e75919af
masum-yasin <masum55549@gmail.com>
2024-04-02 17:17:45 +0600

    update

--- laravel-hotel-management/app/Http/Controllers/AdminController.php
+++ laravel-hotel-management/app/Http/Controllers/AdminController.php
@@ -2,11 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
         return view('admin.body.index');
+    }
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('admin.login');
+    }
+
+    public function AdminLogin(){
+        return view('admin.admin_login');
+    }
+    public function AdminProfile(){
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view',compact('adminData'));
     }
 }
--- laravel-hotel-management/routes/web.php
+++ laravel-hotel-management/routes/web.php
@@ -34,6 +34,11 @@ require __DIR__.'/auth.php';
 // Admin Group MiddleWare
 Route::middleware(['auth','roles:admin'])->group(function(){
     Route::get('/admin/dashboard',[AdminController::class,'AdminDashboard'])->name('admin.dashboard');
+    Route::get('admin/logout',[AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('admin/profile',[AdminController::class, 'AdminProfile'])->name('admin.profile');
 });
 // End Admin Middleware//
+
+// Admin Login//
+Route::get('/admin/login',[AdminController::class,'AdminLogin'])->name('admin.login');
 
