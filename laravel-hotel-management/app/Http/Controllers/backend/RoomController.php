4f42dfb2a4810d72382f517f85410ecdc8d9f30c
masum-yasin <masum55549@gmail.com>
2024-04-30 01:11:07 -0700

    update

--- laravel-hotel-management/app/Http/Controllers/backend/RoomController.php
+++ laravel-hotel-management/app/Http/Controllers/backend/RoomController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Facility;
 use App\Models\Room;
 use Illuminate\Http\Request;
 
 class RoomController extends Controller
 {
     public function EditRoom($id){
+        $basic_facility =Facility::where('room_id',$id)->get();
         $editData =Room::find($id);
-        return view('backend.allroom.room.edit_rooms',compact('editData'));
+        return view('backend.allroom.room.edit_rooms',compact('editData','basic_facility'));
     }
 }
